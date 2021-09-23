583544083b1cad652a02627a1661e99c13b72b9d
Peres, Ronaldo <ronaldo_peres@dell.com>
2021-09-23 18:08:10 -0300

    add day3 - fibonacci

---
+++ DAY3/Fibonacci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace day3
+{
+    public static class Fibonacci
+    {
+        public static void Fibo(int num)
+        {
+            int val1 = 0, val2 = 1, val3, i;            
+            
+            Console.WriteLine("\nFibonacci series:");
+            Console.Write(val1 + " " + val2 + " ");
+            for (i = 2; i < num; ++i)
+            {
+                val3 = val1 + val2;
+                Console.Write(val3 + " ");
+                val1 = val2;
+                val2 = val3;
+            }            
+        }
+    }
+}
