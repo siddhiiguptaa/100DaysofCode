a15a8b6bc5dbdfa2c3c818e58652002c5ffd45b7
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-23 22:02:37 +0530

    achived singleStep

--- DAY4/stairsPattern.py
+++ DAY4/stairsPattern.py
@@ -6,15 +6,30 @@ level: medium
 ******
 """
 
-def stairsPattern(num):
+
+def singleStairsPattern(num):
     # required for a step of the stair
-    height = ((num * 3) + 1)
+    height = (num * 6)
     width = (num * 6)
 
-    # for a single step 
-    for i in range(height, 0, -1):
-        for j in range(i):
-            print ("*", end=" ")
+    # for a single step
+    for i in range(height - 1, height - 2, -1):
+
+        for j1 in range(i):
+            print(" ", end=" ")
+        print("*", end="")
         print()
 
-stairsPattern(1)
+        for j2 in range(i):
+            print(" ", end=" ")
+        print("*", end="")
+        print()
+
+        for j4 in range(6, 0, -1):
+            print("*", end=" ")
+        print()
+
+
+singleStairsPattern(1)
+
+# stairsPattern(1)
