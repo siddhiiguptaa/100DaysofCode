73a9cc6ac0fbbfc68b7d8d427d81c47a101090b4
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-23 20:35:57 +0530

    update added rows

--- DAY4/stairsPattern.py
+++ DAY4/stairsPattern.py
@@ -1,4 +1,6 @@
 """
+hackerRank technical interview question
+level: medium
      *
      * 
 ******
@@ -6,5 +8,13 @@
 
 def stairsPattern(num):
     # required for a step of the stair
-    height = num * 3
-    width = num * 6
+    height = ((num * 3) + 1)
+    width = (num * 6)
+
+    # for a single step 
+    for i in range(height, 0, -1):
+        for j in range(i):
+            print ("*", end=" ")
+        print()
+
+stairsPattern(1)
