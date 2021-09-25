797ebfbcad05f40e8486aaee3e19c61ea535bc75
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-25 23:41:30 +0530

    added solution

--- DAY6/evenFibonacciNumbers.js
+++ DAY6/evenFibonacciNumbers.js
@@ -10,3 +10,21 @@
  * find the sum of the even-valued terms.
 
  */
+
+const fiboEvenSum = (number) => {
+  if (number <= 1) {
+    return 0;
+  } else {
+    let evenSum = 0,
+      prevFibNum = 1,
+      // According to problem description our Fibonacci series starts with 1, 2
+      fibNum = 2; 
+    for (let i = 2; fibNum <= number; i++) {
+      if (fibNum % 2 == 0) {
+        evenSum += fibNum;
+      }
+      [prevFibNum, fibNum] = [fibNum, prevFibNum + fibNum];
+    }
+    return evenSum;
+  }
+};
