734e5649a7c89e43b6e3390a96a9bbc10ce4ed5a
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-26 00:45:04 +0530

    Update 100Doors.js
    added code explanation

--- DAY5/100Doors.js
+++ DAY5/100Doors.js
@@ -22,3 +22,17 @@ function getFinalOpenedDoors(numDoors) {
 
   return doors;
 }
+
+// code explanation 
+/**
+ * Each door is open if it is visited an odd number of times. 
+ * Each door is visited once for each number that divides into it. 
+ * Therefore, each open door has an odd number of divisors.
+
+ * But divisors come in pairs: a / b === c 
+ * means that a === b * c and b and c divide evenly into a .
+
+ * So the only way to have an odd number of divisors is 
+ * if for one of the pairs of divisors b === c , 
+ * which means a === b * b is a perfect square.
+ */
