f43257c9a48fbca47305a2cf23de0554c95f9ad1
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-26 04:54:15 +0530

    created server.js

--- DAY7/MERN_CRUD_app/server/server.js
+++ DAY7/MERN_CRUD_app/server/server.js
@@ -1,1 +1,27 @@
 const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+// application invoking express in app
+const app = express()
+
+// middleware request and response 
+app.use(cors())
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+// routes
+
+app.get('*', (req, res) => {
+    res.json({
+        data:'you received  nodejs api for react node crud app'
+    })
+})
+
+// port variable
+const port = process.env.PORT || 8000
+app.listen(port, () => console.log(`The server is running on port ${port}`))
