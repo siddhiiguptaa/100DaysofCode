5566b2c326c62482f7e8361e1fd62c8bd29b6ef4
yashasvi sakure <yashasvisakure@gmail.com>
2021-09-26 08:46:03 +0530

    Done for day 6

---
+++ DAY6/leap_year.java
@@ -0,0 +1,22 @@
+
+public class leap_year{
+    public static void main(String args[]){
+            int year = 2002;
+            boolean leap = false;
+            if(year%4==0)
+                leap = true;
+                else
+                leap = false;
+                if(leap)
+                System.out.println("It is Leap Year");
+                else
+                System.out.println("It is not Leap Year");
+            
+            
+                
+                
+            
+            
+        
+    }
+}
