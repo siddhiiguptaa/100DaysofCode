a1d8e16dd888fc7d1bed778ecc1993fdfc5b505f
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-27 15:52:30 +0530

    added Nav Bar with Create

--- DAY7/MERN_CRUD_app/client/src/App.js
+++ DAY7/MERN_CRUD_app/client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Nav from './Nav'
 // import { BrowserRouter as Router} from "react-router-dom";
 
 
 export default function App() {
   return (
 
-    <div className="container p-5">
-      <h1>MERN CRUD App</h1>
+    <div className="container pb-5">
+      <Nav/>
+      <h1>
+        MERN CRUD App
+      </h1>
     </div>
  
   )
