2cf5240923fcc5ecaa48aed6e0ab3993cc6f6aca
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-09-30 07:57:31 +0530

    added  react-syntax-highlighter

--- DAY10/liveMarkdownPreview/markdown-preview/src/App.js
+++ DAY10/liveMarkdownPreview/markdown-preview/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import React,{ useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function App() {
+  const [input,setInput] = useState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea 
+        className="textarea" 
+        value={ input } 
+        onChange={ 
+          (e) => setInput(e.target.value)
+        }
+        />
+        <ReactMarkdown 
+          children= { input } 
+          className="markdown"
+          components ={{code : Component}}/>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+const Component = (value, language) => {
+  return (
+    <SyntaxHighlighter language={language ?? null} style={docco}>
+      {value ?? null}
+    </SyntaxHighlighter>
+  );
+};
