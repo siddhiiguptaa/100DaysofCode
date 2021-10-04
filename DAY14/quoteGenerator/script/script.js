aea70021985bfadc315c0d9d92c3af0b4f64bfb1
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-04 08:39:49 +0530

    fix quotes font

--- DAY14/quoteGenerator/script/script.js
+++ DAY14/quoteGenerator/script/script.js
@@ -1,8 +1,8 @@
-const quoteContainer =document.getElementById('quote-container');
-const quoteText =document.getElementById('quote');
-const authorText =document.getElementById('author');
-const twitterBtn =document.getElementById('twitter');
-const newQuotesBtn =document.getElementById('new-quote');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuotesBtn = document.getElementById('new-quote');
 
 // get quotes from an API
 
@@ -12,9 +12,22 @@ let apiQuotes = []
 function newQuotes(){
     // pick a random quote 
     const quotes = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quotes.author;
-    quoteText.textContent = quotes.text;
     // console.log(quotes);
+    if(!quotes.author){
+        authorText.textContent = "unknown";
+    }
+    else{
+        authorText.textContent = quotes.author;
+    }
+    // check quote length to determine styling 
+    if (quotes.text.length > 50){
+        quoteText.classList.add('long-quote');
+    }
+    else{
+        quoteText.classList.remove('long-quote');
+    }
+        quoteText.textContent = quotes.text;
+
 }
 async function getQuotes() {
     const apiURL = 'https://type.fit/api/quotes'
