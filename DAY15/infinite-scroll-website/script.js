5a332e09f56ea64945f25d369868da76ddaed073
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-05 08:38:08 +0530

    created scripts
    added image api function flow

---
+++ DAY15/infinite-scroll-website/script.js
@@ -0,0 +1,23 @@
+// require('dotenv').config(); 
+
+const count = 2
+const API_KEY = process.env.API_KEY
+// unsplash request url
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${count}`
+
+// getting photos using the api key
+
+async function getPhotos(){
+    try {
+        const response = await fetch(apiURL)
+        const data = await response.json()
+        console.log(data)
+    }
+    catch (err){
+        console.log(`The following error occurred: ${err.message}`)
+    }
+}
+
+// on page load
+
+getPhotos()
