0f05f06c6b88ff8d31b2e00749bedd9b13ddea3f
yashasvi sakure <yashasvisakure@gmail.com>
2021-10-05 07:47:46 +0530

    Added simple java problem

---
+++ DAY14/greeting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class greeting {
+    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        
+        String n = in.nextLine(); 
+        
+            System.out.print("Hii " + n + " Have a Nice Day");
+        
+        in.close();
+    }
+}
+
