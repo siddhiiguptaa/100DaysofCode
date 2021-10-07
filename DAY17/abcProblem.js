67dcc4c878c6219cdfbbf2258d537943b8e49447
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-07 08:05:06 +0530

    Update abcProblem.js
    solution attempt I

--- DAY17/abcProblem.js
+++ DAY17/abcProblem.js
@@ -36,3 +36,23 @@
 
 
  */
+
+
+
+function canMakeWord(word) {
+      const blocksArr = [
+    ["B", "O"], ["X", "K"], ["D", "Q"], ["C", "P"], ["N", "A"],
+    ["G", "T"], ["R", "E"], ["T", "G"], ["Q", "D"], ["F", "S"],
+    ["J", "W"], ["H", "U"], ["V", "I"], ["A", "N"], ["O", "B"],
+    ["E", "R"], ["F", "S"], ["L", "Y"], ["P", "C"], ["Z", "M"],
+  ]
+
+  word = word.toUpperCase().split('')
+  for(let letter  in word){
+      const blockIndex = blocksArr.findIndex(
+          (block) => block.indexOf(word[letter]) >= 0
+      )
+        (blockIndex === -1) ? false : blocksArr.splice(blockIndex,1)
+  }
+  return true
+}
