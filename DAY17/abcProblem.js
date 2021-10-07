4c1b4882f437c01d77ce6cc92b00251d91bc79e3
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-07 07:55:01 +0530

    Update abcProblem.js
    the rules of the game

--- DAY17/abcProblem.js
+++ DAY17/abcProblem.js
@@ -1,4 +1,38 @@
 /**
  * ABC Problem
+ * 
+ * You are given a collection of ABC blocks (e.g., childhood alphabet blocks). 
+ * There are 20 blocks with two letters on each block. 
+ * A complete alphabet is guaranteed amongst all sides of the blocks. 
+ * The sample collection of blocks:
+ * 
+    (B O)
+    (X K)
+    (D Q)
+    (C P)
+    (N A)
+    (G T)
+    (R E)
+    (T G)
+    (Q D)
+    (F S)
+    (J W)
+    (H U)
+    (V I)
+    (A N)
+    (O B)
+    (E R)
+    (F S)
+    (L Y)
+    (P C)
+    (Z M)
+
+ * Implement a function that takes a string (word) and determines 
+    whether the word can be spelled with the given collection of blocks.
+
+    * Some rules to keep in mind:
+        Once a letter on a block is used, that block cannot be used again.
+        The function should be case-insensitive.
+
 
  */
