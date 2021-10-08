25119b9ed51056f7fa5c640715ceefde4d5974f5
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-10-08 08:14:39 +0530

    Update babbageProblem.js
    problem definition

--- DAY18/babbageProblem.js
+++ DAY18/babbageProblem.js
@@ -1,3 +1,20 @@
 /**
  * Babbage problem
+ * 
+ * Charles Babbage, looking ahead to the sorts of problems his Analytical Engine 
+ * would be able to solve, gave this example:
+ * 
+ * What is the smallest positive integer whose square ends in the digits 269,696?
+ - Babbage, letter to Lord Bowden, 1837; see Hollingdale and Tootill, Electronic Computers, 
+   second edition, 1970, p. 125.
+ * 
+ * He thought the answer might be 99,736, whose square is 9,947,269,696; 
+ * but he couldn't be certain.
+ * 
+ * The task is to find out if Babbage had the right answer.
+ * 
+ * Implement a function to return the lowest integer that 
+ * satisfies the Babbage problem. If Babbage was right, return Babbage's number.
  */
+
+   
