34f549f46f278082bad3bcac015708b569ba7054
yashasvi sakure <yashasvisakure@gmail.com>
2021-10-10 22:25:16 +0530

    solved a median question

---
+++ DAY20/median_of_sorted_array.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+int m;
+int arr1[n];
+cin >> arr[n];
+int arr2[m];
+cin >> arr[n];
+vector<int> sum;
+sum[m+n] = arr1[n]+arr2[m];
+sort(sum.begin(), sum.end());
+if((m+n) % 2 != 0){
+    cout << (m+n )/2;
+}
+    cout << ((((m+n))-1)+(m+n))/2;
+}
