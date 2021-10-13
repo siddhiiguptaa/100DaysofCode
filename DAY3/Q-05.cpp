2ab43a9849fd2192b1f0312a4c3b78911f65d5a4
Sandhya312 <85842071+Sandhya312@users.noreply.github.com>
2021-10-13 23:36:44 +0530

    Day-03
    Sandhya

---
+++ DAY3/Q-05.cpp
@@ -0,0 +1,15 @@
+//C++ Program to Find Size of int, float, double and char
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	cout<<"Size of integer is:"<<sizeof(int)<<endl
+	    <<"Size of float is:"<<sizeof(float)<<endl
+	    <<"Size of Char is:"<<sizeof(char)<<endl
+	    <<"Size of double is:"<<sizeof(double)<<endl;
+	
+	
+	return 0;
+}
---
+++ DAY3/Q-06.cpp
@@ -0,0 +1,16 @@
+//C++ Program to Multiply two Numbers
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	cout<<"Enter the value of a and b\n";
+	cin>>a>>b;
+	cout<<"Multiplication of a and b is :"<<(a*b);
+	
+	
+	
+	return 0;
+}
