493f3456405ea110ea8f9c03ee1b607183da90ec
yashasvi sakure <yashasvisakure@gmail.com>
2021-10-15 21:36:03 +0530

    Solved Graph question

---
+++ DAY25/graph.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n , m;
+    cin >> n>>m;
+    int adj[n+1][n+1];
+    for(int i=0;i<n;i++){
+        int u, v;
+        cin >> u>> v;
+    adj[u][v] =1;
+    adj[u][v] =1;
+    } 
+    return 0;
+}
