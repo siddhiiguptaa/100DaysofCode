decf98ad0c16d64fa7a4846a1cf6c212d6d703c2
yashasvi sakure <yashasvisakure@gmail.com>
2021-10-18 17:41:38 +0530

    Solved GFG questions

---
+++ DAY28/getsum.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    int getSum(int a[], int n) {
+        int sum =0 ;
+        for(int i=0;i<n;i++){
+            sum = sum + a[i];
+            
+        }
+        return sum;
+        // Your code goes here
+    }   
+};
