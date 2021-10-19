b71ea9457c5f85f4af2f410519b932f0e520f842
yashasvi sakure <yashasvisakure@gmail.com>
2021-10-19 18:55:47 +0530

    Solved questions

--- DAY28/largest_element_inarray.cpp
+++ DAY28/largest_element_inarray.cpp
@@ -3,10 +3,10 @@ class Solution
 public:
     int largest(vector<int> &arr, int n)
     {
-        int max =0;
+        int  max=0;
     for(int i=0;i<n;i++){
         if(arr[i] > max)
-        max = arr[i];
+      max= arr[i];
     }
         
         
