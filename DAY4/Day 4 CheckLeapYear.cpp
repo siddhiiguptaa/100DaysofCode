7f4dee976d884eec100691c32f02859068d87463
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2021-10-23 15:00:43 +0530

    Create Day 4 CheckLeapYear.cpp

---
+++ DAY4/Day 4 CheckLeapYear.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+
+    cout<< "Enter a year";
+    cin>> year;
+
+    if (year %4==0) {
+        if (year %100=0)
+        if (year %400=0)
+        cout<< year << "is an leap year"; {
+
+        else
+        cout<< year << "is not an leap year"; {
+
+        else
+        cout<< year << "is a an leap year"; {
+
+        else
+        cout<< year << "is not an leap year";
+
+
+     return 0;
+    }
