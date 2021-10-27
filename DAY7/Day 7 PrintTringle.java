5800d3b8c925ec11447b1f025c7a8a9de00d7f46
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2021-10-27 17:24:54 +0530

    Create Day 7 PrintTringle.java

---
+++ DAY7/Day 7 PrintTringle.java
@@ -0,0 +1,17 @@
+public class PrintTringle {
+    public static void main(String[] args){
+
+        //METHED1
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+
+        //METHED2
+        //System.out.println("*\n**\n***\n****\n*****");
+    
+    }
+    
+}
