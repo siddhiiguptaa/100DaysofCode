9eeb6fcdb5a759d2f629e30bdb0d67f36fc4ade5
Ronaldo Peres <ronaldoperes@gmail.com>
2021-10-29 20:10:44 -0300

    Update BinarySearch.cs

--- DAY33/BinarySearch.cs
+++ DAY33/BinarySearch.cs
@@ -1,3 +1,9 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
 public class Solution {
     public int Search(int[] nums, int target) {        				
         int num = -1;
