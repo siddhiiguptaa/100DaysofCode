5b8f9c5583210ec2ab0bf563eff37fe950cdb8ae
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2021-10-29 18:30:53 +0530

    Create Day 9 GreaterSmaller.java

---
+++ DAY9/Day 9 GreaterSmaller.java
@@ -0,0 +1,23 @@
+public class GreaterSmaller {
+
+    public static void main(String[] args){
+        int n1 = 20;
+        int n2 = 30;
+
+       if(n1 == n2){
+
+        System.out.println(n1 + "is equal to" + n2);
+       }
+       else{
+           if(n1 > n2){
+
+            System.out.println(n1 + "is greater to" + n2);
+           }
+           else{
+            System.out.println(n1 + "is smaller to" + n2);
+           }
+       }
+
+    }
+    
+}
