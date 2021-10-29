366b0f4adbed16aedee805edf1095d248b37c12d
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2021-10-29 18:34:23 +0530

    Create Day 9 GreaterSmallerMethed2.java

---
+++ DAY9/Day 9 GreaterSmallerMethed2.java
@@ -0,0 +1,24 @@
+public class GreaterSmallerMethed2 {
+    
+    public static void main(String[] args){
+
+        int n1 = 20;
+        int n2 = 30;
+
+       if(n1 == n2){
+
+        System.out.println(n1 + "is equal to" + n2);
+       }
+       else if(n1 > n2){
+        
+        System.out.println(n1 + "is greater to" + n2);
+
+       }
+       else{
+
+        System.out.println(n1 + "is smaller to" + n2);
+       }
+
+    }
+    
+}
