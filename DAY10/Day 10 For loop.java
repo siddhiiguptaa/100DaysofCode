a0a4794062a3d95e5f05e296963bec4ca263ccbd
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2021-10-30 18:37:55 +0530

    Create Day 10 For loop.java

---
+++ DAY10/Day 10 For loop.java
@@ -0,0 +1,13 @@
+public class ForLoopsDemo {
+
+    public static void main(String[] args){
+
+        for(int i = 1;  i <= 9  i++){
+
+            System.out.println(i);
+        }
+
+        System.out.println("Done");
+    }
+    
+}
